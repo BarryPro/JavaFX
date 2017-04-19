fb8874aa61a702accf41268de221117aef91087d
belong <belong.belong@outlook.com>
2017-04-19 11:02:03 +0800

    修改NIO的内容 ============ 添加只读缓冲区 ============ 新添加JavaFX的界面项目

---
+++ src/sample/Main.java
@@ -0,0 +1,39 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import sample.settings.Config;
+
+/**
+ * @author belong
+ * @description JavaFX 的程序的入口
+ */
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        // 加载资源文件的界面配置文件
+        Parent root = FXMLLoader.load(getClass().getResource("config/sample.fxml"));
+        // 设置标题
+        primaryStage.setTitle(Config.TITLE);
+        // 设置界面的大小
+        primaryStage.setScene(new Scene(root, 400, 275));
+
+        primaryStage.getIcons();
+        // 显示界面
+        primaryStage.show();
+    }
+
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/sample/settings/Config.java
@@ -0,0 +1,10 @@
+package sample.settings;
+
+/**
+ * @Author: belong.
+ * @Description: 项目用到的全局静态字符串的文件
+ * @Date: 2017/4/19.
+ */
+public class Config {
+    public static final String TITLE = "准生产环境与模型保持一致检查";
+}
